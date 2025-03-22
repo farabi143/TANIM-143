2e4e2a953c0abe99ba147817c6ef98c4c24b9034
TANIM.143 <penelf7@gmail.com>
2025-03-22 14:04:05 +0600

    Update TEST-143.py

--- TEST-143.py
+++ TEST-143.py
@@ -1,6 +1,6 @@
 import os
-import Random
-Random.login()
+import Rndom
+Rndom.login()
 #--------------{Introduction}------------#
 #---পারলে ডিকোড করো না পারলে মাদারচো*দ তুই--#
 #--------------{Introduction}------------#
