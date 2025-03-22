7ec312ee67d97206694329c5377175ba44c1152a
TANIM.143 <penelf7@gmail.com>
2025-03-22 13:53:53 +0600

    Update TEST-143.py

--- TEST-143.py
+++ TEST-143.py
@@ -1,6 +1,6 @@
 import os
-import test
-test.login()
+import Random
+Random.login()
 #--------------{Introduction}------------#
 #---পারলে ডিকোড করো না পারলে মাদারচো*দ তুই--#
 #--------------{Introduction}------------#
